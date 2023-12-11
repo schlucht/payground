ecc01e27ed9448d57cdb6afa87357f6e84262604
Schmid Lothar <web@jagolo.ch>
2023-12-11 08:07:11 +0000

    Neuer Test

--- public/login/Api/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('SECRETE_KEY', 'test123');
-
-define('DATABASE', 'database');
-define('DB_USER', 'root');
-define('DB_PASSWORD', 'schlucht');
--- public/login/Api/functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-function sh($stuff, $die=false) {
-    "<pre>{$stuff}</pre>";
-    $die ?? die; 
-}
